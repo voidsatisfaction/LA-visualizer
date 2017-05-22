8201584049eb4ca07f6f2391e310a6be77988a31
voidSatisfaction <lourie@naver.com>
2017-05-22 14:24:56 +0900

    feat: add rechart and make room for navbar

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Search from './components/page/Search';
 import NotFound from './components/page/NotFound';
 
 import Header from './components/organism/Header';
+import NavBar from './components/organism/NavBar';
 
 export default class App extends Component {
   state = {  }
@@ -18,14 +19,17 @@ export default class App extends Component {
       <Router>
         <div>
           <Header />
+          <NavBar />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/page1/:username" component={Page1} />
-            <Route path="/page2" component={Page2} />
-            <Route path="/login" component={Login} />
-            <Route path="/me" component={MyPage} />
-            <Route path="/search" component={Search} />
-            <Route component={NotFound} />
+            <div style={{ marginLeft: '20rem' }}>
+              <Route exact path="/" component={Home} />
+              <Route path="/page1/:username" component={Page1} />
+              <Route path="/page2" component={Page2} />
+              <Route path="/login" component={Login} />
+              <Route path="/me" component={MyPage} />
+              <Route path="/search" component={Search} />
+              <Route component={NotFound} />
+            </div>
           </Switch>
         </div>
       </Router>
---
+++ src/style/color.js
@@ -0,0 +1,6 @@
+const color = {
+  primary: "#5c7cfa",
+  header: "rgb(27, 28, 29)"
+}
+
+export default color;
