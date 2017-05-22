f59e65cf4623fbd6e2c7ca6074266fde4c8b0430
voidSatisfaction <lourie@naver.com>
2017-05-22 22:41:39 +0900

    feat: add subtitle of data and font override

--- src/components/page/Home/index.jsx
+++ src/components/page/Home/index.jsx
@@ -1,41 +1,32 @@
 import React, { Component } from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import { Icon } from 'semantic-ui-react';
 
 import './style.css';
 
-const data = [
-      {name: 'A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-      {name: 'G', uv: 3490, pv: 4300, amt: 2100},
-]
+const MAX = 40;
+const data = [];
+for (var i = 1; i <= MAX; i++) {
+  data.push({
+    name: i,
+    uv: Math.floor(Math.random() * 4000),
+    pv: Math.floor(Math.random() * 4000) 
+  });
+}
 
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
         <div className="home-chart">
-          <h1 className="home-title">Bookroll 閲覧数</h1>
+          <h3 className="home-title">Bookroll 閲覧数</h3>
+          <div className="home-title-sub">
+            <Icon name="calendar" />2017.4.1 ~ 2017.5.22
+          </div>
           <LineChart width={1000} height={400} data={data} margin={{ top: 5, right: 30, bottom: 5, left: 0 }}>
             <Line type="monotone" dataKey="pv" stroke="#8884d8" />
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <Tooltip />
             <XAxis dataKey="name" />
             <YAxis />
           </LineChart>
