2bef24aa681be95a149d5aa04f22b98caa4b0422
voidSatisfaction <lourie@naver.com>
2017-05-22 17:38:29 +0900

    feat: rough design on side bar

--- src/components/organism/NavBar/index.jsx
+++ src/components/organism/NavBar/index.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './style.css';
 
 export default class NavBar extends Component {
   render() {
     return (
-      <div>
-        <h1>this is navbar</h1>
+      <div className="vertical-menu">
+        <NavLink exact to="/" activeClassName="active">Home</NavLink>
+        <NavLink to="/page1">Link1</NavLink>
+        <NavLink to="#">Link2</NavLink>
+        <NavLink to="#">Link3</NavLink>
+        <NavLink to="#">Link4</NavLink>
       </div>
     );
   }
