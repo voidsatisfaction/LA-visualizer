7cb1086c4ab420cc5b4a4586455d8db02e99695e
voidSatisfaction <lourie@naver.com>
2017-05-22 18:26:21 +0900

    feat: add icons on side navbar using semantic ui

--- src/components/organism/NavBar/index.jsx
+++ src/components/organism/NavBar/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
 
 import './style.css';
 
@@ -7,8 +8,14 @@ export default class NavBar extends Component {
   render() {
     return (
       <div className="vertical-menu">
-        <NavLink exact to="/" activeClassName="active">Home</NavLink>
-        <NavLink to="/page1">Link1</NavLink>
+        <NavLink exact to="/" activeClassName="active item">
+          <Icon className="item-icon" name="home" size="large" />
+          <span className="item-text" >ホーム</span>
+        </NavLink>
+        <NavLink to="/page2">
+          <Icon className="item-icon" name="tasks" size="large" />
+          <span className="item-text" >タスク</span>
+        </NavLink>
         <NavLink to="#">Link2</NavLink>
         <NavLink to="#">Link3</NavLink>
         <NavLink to="#">Link4</NavLink>
